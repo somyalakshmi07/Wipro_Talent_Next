1b432b1e47b7a946059eb9baabb5a08141c68073
somyalakshmi07 <somyalakshmi18@gmail.com>
2025-07-28 12:29:48 +0530

    factotial

--- fabwithoutreccursive.java
+++ fabwithoutreccursive.java
@@ -1,21 +1,19 @@
-// program to write fibonacci series using recursion
-import java.util.Scanner;
-public class fibonacc {
+// program to find the nth Fibonacci number
+import java.util.Scanner;   
+public class fabwithoutreccursive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of terms in the Fibonacci series: ");
         int n = sc.nextInt();
         System.out.println("Fibonacci series up to " + n + " terms:");
+        
+        int a = 0, b = 1;
         for (int i = 0; i < n; i++) {
-            System.out.print(fibonacci(i) + " ");
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
         }
         sc.close();
-    }
-
-    public static int fibonacci(int num) {
-        if (num <= 1) {
-            return num;
-        }
-        return fibonacci(num - 1) + fibonacci(num - 2);
     }
 }
